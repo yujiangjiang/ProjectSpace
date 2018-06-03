2388484e5750d94e0f449531b29fa280b8729cfe
yujiangjiang <314130446@qq.com>
2018-06-03 15:01:29 +0800

    新增thymeleaf项目

---
+++ thymeleafProject/src/main/java/com/yu/thymeleafProject/ThymeleafProjectApplication.java
@@ -0,0 +1,12 @@
+package com.yu.thymeleafProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafProjectApplication.class, args);
+	}
+}
